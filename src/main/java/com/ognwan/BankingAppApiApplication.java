c1fb7b507f4c3332151a77b0396c4a41eca14347
Gerald Nwanekezie <gnwanek@gmail.com>
2022-11-03 16:11:46 -0600

    Initial commit

---
+++ src/main/java/com/ognwan/BankingAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.ognwan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ognwan/BankingAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ognwan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
