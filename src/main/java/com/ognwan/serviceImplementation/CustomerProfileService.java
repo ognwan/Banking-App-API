7dee57b3cc129eff57c3687b68fa55739f0035ac
Gerald Nwanekezie <gnwanek@gmail.com>
2022-11-04 11:41:43 -0600

    fixed compile error

--- src/main/java/com/ognwan/serviceImplementation/CustomerProfileService.java
+++ src/main/java/com/ognwan/serviceImplementation/CustomerProfileService.java
@@ -75,6 +75,6 @@ public class CustomerProfileService implements ServiceInterface<CustomerProfile>
 
 	public boolean validate(String email, String password) {
 		CustomerProfile returnedCustomer = customerProfileRepo.findByEmail(email);
-		return returnedCustomer != null && returnedCustomer.getPassword().equals(password)
+		return returnedCustomer != null && returnedCustomer.getPassword().equals(password);
 	}
 }
