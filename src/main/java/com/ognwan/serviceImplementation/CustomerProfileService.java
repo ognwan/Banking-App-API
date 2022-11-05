6d39c8abc22586b6b65dca5ec4aaa6857bb18238
Gerald Nwanekezie <gnwanek@gmail.com>
2022-11-04 22:30:24 -0600

    created update profile endpoint

--- src/main/java/com/ognwan/serviceImplementation/CustomerProfileService.java
+++ src/main/java/com/ognwan/serviceImplementation/CustomerProfileService.java
@@ -101,6 +101,10 @@ public class CustomerProfileService implements ServiceInterface<CustomerProfile>
 		throw new Exception("username or password incorrect");
 	}
 
+	public CustomerProfile getCustomerByEmail(String email) {
+		return customerProfileRepo.findByEmail(email);
+	}
+
 	public boolean isPasswordValid(String password) {
 		Pattern pattern = Pattern.compile(
 				"^(?=[a-zA-Z0-9!@#$%^&*_=+;:,.?]{8,}$)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[!@#$%^&*_=+;:,.?]).*");
